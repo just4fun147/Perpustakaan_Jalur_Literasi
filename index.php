45e9e8ef295ea35d88acce1882a581fa765ce483
just4fun147 <pauluswindito1@gmail.com>
2022-11-06 12:46:53 +0700

    firstCommit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="style.css" rel="stylesheet">
+    <title>Perpustakaan Jalur Literasi</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="/uts_paw"><img src="../assets/images/logo.png"
+                    alt="Logo Jalur Literasi" width="180px" height="50px"></a>
+            <div class="d-flex justify-content-end">
+                <button class="btn me-2" type="button" style="background-color:#ff4e44">
+                    <a class=" text-light" style="text-decoration: none" href="./page/registerPage.php">Sign Up</a>
+                </button>
+                <button class="btn btn-outline-warning ms-2" type="button" href="./loginPage.php">
+                    <a class="text-light" style="text-decoration: none" href="./page/loginPage.php">Login</a>
+                </button>
+            </div>
+        </div>
+    </nav>
+
+    <div class="bg">
+        <div class="container min-vh-100 d-flex align-items-center">
+            <div class="mt-5 shadow-lg p-2 bg-dark rounded">
+                <h1 class="text-center align-middle text-white">
+                    <b class=""> Selamat Datang di Perpustakaan Jalur Literasi</b>
+                </h1>
+                <p class="align-middle text-white"> Menciptakan Generasi Literat Melalui Perpustakaan</p>
+            </div>
+        </div>
+    </div>
+
+    <footer class="bg-dark text-center text-white">
+        <div class="container p-4 pb-0">
+            
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-facebook-f"></i></a>
+
+                <!-- Twitter -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-twitter"></i></a>
+
+                <!-- Instagram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-instagram"></i></a>
+
+            </section>
+        </div>
+
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright: Paulus Pandu Windito
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
